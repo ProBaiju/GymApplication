505cddc821d0cf503d75be2b49ac8a7f177ea67c
ProBaiju <81493624+ProBaiju@users.noreply.github.com>
2023-06-20 12:19:39 +0530

    Add files via upload

---
+++ Gymapplication/Gymapplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gymapplication
+//
+//  Created by AL20 on 23/02/23.
+//  Copyright © 2023 AL20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
